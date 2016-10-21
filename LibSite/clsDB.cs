461380a7c3302467f7f220ab354753a220323443
khuthadzotshikotshi <khuthadzotshikotshi@gmail.com>
2016-10-21 15:21:25 +0200

    fix solution missing error

---
+++ LibSite/clsDB.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Data;
+
+namespace LibSite
+{
+    public class clsDB
+    {
+        private OleDbConnection db_conn = new OleDbConnection();
+        private OleDbCommand comm = new OleDbCommand();
+        private string query;
+        private static string _userName, _userType;
+        public clsDB()
+        {
+            comm.Connection = db_conn;
+        }
+        public string Query
+        {
+            protected get
+            {
+                return query;
+            }
+            set
+            {
+                query = value;
+            }
+        }
+        public OleDbCommand command
+        {
+            get
+            {
+                return comm;
+            }
+            set
+            {
+                comm = value;
+            }
+        }
+        public string userName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                _userName = value;
+            }
+        }
+        public string userType
+        {
+            get
+            {
+                return _userType;
+            }
+            protected set
+            {
+                _userType = value;
+            }
+        }
+        public void connDB(string db_loc)
+        {
+            string conn_str = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + db_loc + ";";
+            try
+            {
+                db_conn.ConnectionString = conn_str;
+                db_conn.Open();
+            }
+            catch (Exception error)
+            {
+                throw error;
+            }
+        }
+        public int db_non_returning_query()
+        {
+            int affctdRows = 0;
+            comm.CommandText = query;
+            try
+            {
+                affctdRows = comm.ExecuteNonQuery();
+            }
+            catch (Exception error)
+            {
+                throw new Exception (error.Message);
+            }
+            finally
+            {
+                db_conn.Close();
+            }
+            return (affctdRows);
+        }
+        public DataTable db_select()
+        {
+            DataTable myTable = new DataTable();
+            comm.CommandText = query;
+            try
+            {
+                myTable.Load(comm.ExecuteReader());
+            }
+            catch(Exception error)
+            {
+                throw error;
+            }
+            finally
+            {
+                db_conn.Close();
+            }
+            return myTable;
+        }
+    }
+}
---
+++ LibSite/clsUserManagement.cs
@@ -0,0 +1,95 @@
+﻿using System.Data;
+using System.Data.OleDb;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LibSite
+{
+    public class clsUserManagement:clsDB
+    {
+        private string _name, _surname;
+        public string name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+        public string surname
+        {
+            get { return _surname; }
+            private set { _surname = value; }
+        }
+        //authenticate the eployee trying to gain access to the system.
+        public bool Login(string UserName, string passKey)
+        {
+            DataTable loginTable;
+            command.Parameters.Add("@UserName", OleDbType.VarChar);
+            command.Parameters.Add("@PassKey", OleDbType.VarChar);
+            command.Parameters["@UserName"].Value = UserName;
+            command.Parameters["@PassKey"].Value = passKey;
+            Query = "SELECT userType, empName, surname FROM Managers WHERE userName = @UserName AND passKey = @PassKey;";
+            loginTable = db_select();
+            if (loginTable.Rows.Count == 1)
+            {
+                userType = loginTable.Rows[0].ItemArray[0].ToString();
+                name = loginTable.Rows[0].ItemArray[1].ToString();
+                surname = loginTable.Rows[0].ItemArray[2].ToString();
+                loginTable.Dispose();
+                return (true);
+            }
+            loginTable.Dispose();
+            return (false);
+        }
+        //change password, only accessible to the currently logged on Manager.
+        public bool changePassKey(string passKey)
+        {
+            command.Parameters.Add("@UserName", OleDbType.VarChar);
+            command.Parameters.Add("@PassKey", OleDbType.VarChar);
+            command.Parameters["@UserName"].Value = userName;
+            command.Parameters["@PassKey"].Value = passKey;
+            Query = "UPDATE Managers SET passKey=@PassKey WHERE userName=@UserName";
+            if (db_non_returning_query() == 1)
+            {
+                return (true);
+            }
+            return (false);
+        }
+        //only accessible to the office Manager
+        //add a new employee to the system.
+        public bool addUser(string UserName, string passKey, string UserType, string name, string surname, string address, string phnumber)
+        {
+            command.Parameters.Add("@UserName", OleDbType.VarChar);
+            command.Parameters.Add("@PassKey", OleDbType.VarChar);
+            command.Parameters.Add("@userType", OleDbType.VarChar);
+            command.Parameters.Add("@name", OleDbType.VarChar);
+            command.Parameters.Add("@surname", OleDbType.VarChar);
+            command.Parameters.Add("@address", OleDbType.VarChar);
+            command.Parameters.Add("@phnumber", OleDbType.VarChar);
+            command.Parameters["@UserName"].Value = UserName;
+            command.Parameters["@PassKey"].Value = passKey;
+            command.Parameters["@userType"].Value = UserType;
+            command.Parameters["@name"].Value = name;
+            command.Parameters["@surname"].Value = surname;
+            command.Parameters["@address"].Value = address;
+            command.Parameters["@phnumber"].Value = phnumber;
+            Query = "INSERT INTO Managers (userName, passKey, userType, empName, surname, address, phnumber) VALUES (@UserName, @PassKey, @userType, @name, @surname, @address, @phnumber);";
+            if (db_non_returning_query() > 0)
+            {
+                return (true);
+            }
+            return (false);
+        }
+        //also only accessible to the office manager.
+        //remove an employee from the system.
+        public bool removeUser(string UserName)
+        {
+            command.Parameters.Add("@UserName");
+            command.Parameters["@UserName"].Value = UserName;
+            Query = "DELETE * FROM Employees WHERE userName = @userName;";
+            if (db_non_returning_query() > 0)
+            {
+                return (true);
+            }
+            return (false);
+        }
+    }
+}
